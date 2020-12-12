ca78ea331513a9a1b29e30802af1b33e10ac175a
mordehaibenharush <ben-harush@campus.technion.ac.il>
2020-12-12 20:37:17 +0200

    12.12.20.

--- my_queue.py
+++ my_queue.py
@@ -1,12 +1,20 @@
+from multiprocessing import Process, Pipe, Lock
+
 
 class MyQueue(object):
 
     def __init__(self):
+        self.lock = Lock()
+        self.write_p, self.read_p = Pipe()
         ''' Initialize MyQueue and it's members.
         '''
-        raise NotImplementedError("To be implemented")
 
     def put(self, msg):
+        self.lock.acquire()
+        try:
+            self.write_p.send(msg)
+        finally:
+            self.lock.release()
         '''Put the given message in queue.
 
         Parameters
@@ -14,14 +22,44 @@ class MyQueue(object):
         msg : object
             the message to put.
         '''
-        raise NotImplementedError("To be implemented")
 
     def get(self):
+        return self.read_p.recv()
         '''Get the next message from queue (FIFO)
             
         Return
         ------
         An object
         '''
-        raise NotImplementedError("To be implemented")
 
+
+def reader_pipe(q):
+    while True:
+        msg = q.get()
+        print(msg)  # Read from the output pipe and do nothing
+        if msg=='DONE':
+            break
+
+
+def writer_pipe(count, q):
+    if count == -1:
+        q.put('DONE')
+    q.put(2*count)             # Write 'count' numbers into the input pipe
+    q.put(2*count + 1)
+    print("*** ", 2 * count)
+    print("*** ", 2 * count + 1)
+
+if __name__ == "__main__":
+    q = MyQueue()
+    reader_p = Process(target=reader_pipe, args=(q,))
+    writers = []
+    for i in range(5):
+        writers.append(Process(target=writer_pipe, args=(i, q)))
+    reader_p.start()  # Launch the reader process
+    for w in writers:
+        w.start()
+
+    for w in writers:
+        w.join()
+    writer_pipe(-1, q)
+    reader_p.join()
